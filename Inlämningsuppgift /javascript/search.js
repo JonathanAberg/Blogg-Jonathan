cec0b85c6761967da29652f4d7ec1e977ea98126
JonathanAberg <jonathan.aberg@hotmail.com>
2024-10-03 11:34:13 +0200

    Implementerat en fungerande sökfunktion samt justerat CSS för att snygga till sökfältets ruta.

--- Inlämningsuppgift /javascript/search.js
+++ Inlämningsuppgift /javascript/search.js
@@ -2,32 +2,35 @@
 const SearchInput = document.getElementById("search-input");
 
 //hämta alla inläggets element
-const postElements = document.querySelectorAll('.post-box');
+const postElements = document.querySelectorAll(".post-box");
 
 //Funktion för att filtrera inläggen baserat på sökfältets inmatning
 function filterPosts(searchTerm) {
-    //konvertera söktermen till lowercase för case-insensitive sökning
-    const lowercaseSearchTerm = SearchTerm.toLowerCase();
+  //konvertera söktermen till lowercase för case-insensitive sökning
+  const lowercaseSearchTerm = searchTerm.toLowerCase();
 
-    //Loopa igenom varje inläggs element
-    postElements.forEach(postElement => {
-        //Hämta inläggets titel och konvertera till lowercase
-        const postTitle = postElement.querySelector('.post-title').textContent.toLowerCase();
+  //Loopa igenom varje inläggs element
+  postElements.forEach((postElement) => {
+    //Hämta inläggets titel och konvertera till lowercase
+    const postTitle = postElement
+      .querySelector(".post-title")
+      .textContent.toLowerCase();
 
-        //Kontrollera om inläggets titel innehåller söktermen
-        if(postTitle.includes(lowercaseSearchTerm)) {
-            //visa inläggets element om söktermen matchar'{
-            postElement.style.display = 'block';
-        } else {
-            //göm inläggets element om söktermen inte matchar
-            postElement.style.display = 'none';
-        }
-    });
+    //Kontrollera om inläggets titel innehåller söktermen
+    if (postTitle.includes(lowercaseSearchTerm)) {
+      //visa inläggets element om söktermen matchar'{
+      postElement.style.display = "block";
+    } else {
+      //göm inläggets element om söktermen inte matchar
+      postElement.style.display = "none";
     }
+  });
+}
 // Lägger till event listener till sökfältets inmatning.
 SearchInput.addEventListener("input", (event) => {
-    //hämta söktermen från sökfältet
-    const searchTerm = event.target.value;
+  //hämta söktermen från sökfältet
+  const searchTerm = event.target.value;
 
-    //Filtrera och visa inläggen baserat på söktermen
-    filterPosts(searchTerm);
+  //Filtrera och visa inläggen baserat på söktermen
+  filterPosts(searchTerm);
+});
