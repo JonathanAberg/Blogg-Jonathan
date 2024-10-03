89c3946a752ae5556d5f4f25f5854d5f8cefda6b
JonathanAberg <jonathan.aberg@hotmail.com>
2024-10-03 10:39:30 +0200

    Påbörjat ett JS för sökfuntion samt små justeringar 	modified:   "Inl\303\244mningsuppgift /css/styles.css" 	modified:   "Inl\303\244mningsuppgift /html/index.html" 	modified:   "Inl\303\244mningsuppgift /html/posts/post1.html" 	modified:   "Inl\303\244mningsuppgift /html/posts/post4.html" 	modified:   "Inl\303\244mningsuppgift /javascript/main.js" 	new file:   "Inl\303\244mningsuppgift /javascript/search.js"

---
+++ Inlämningsuppgift /javascript/search.js
@@ -0,0 +1,33 @@
+//Hämta Sökfunktionens inmatings element
+const SearchInput = document.getElementById("search-input");
+
+//hämta alla inläggets element
+const postElements = document.querySelectorAll('.post-box');
+
+//Funktion för att filtrera inläggen baserat på sökfältets inmatning
+function filterPosts(searchTerm) {
+    //konvertera söktermen till lowercase för case-insensitive sökning
+    const lowercaseSearchTerm = SearchTerm.toLowerCase();
+
+    //Loopa igenom varje inläggs element
+    postElements.forEach(postElement => {
+        //Hämta inläggets titel och konvertera till lowercase
+        const postTitle = postElement.querySelector('.post-title').textContent.toLowerCase();
+
+        //Kontrollera om inläggets titel innehåller söktermen
+        if(postTitle.includes(lowercaseSearchTerm)) {
+            //visa inläggets element om söktermen matchar'{
+            postElement.style.display = 'block';
+        } else {
+            //göm inläggets element om söktermen inte matchar
+            postElement.style.display = 'none';
+        }
+    });
+    }
+// Lägger till event listener till sökfältets inmatning.
+SearchInput.addEventListener("input", (event) => {
+    //hämta söktermen från sökfältet
+    const searchTerm = event.target.value;
+
+    //Filtrera och visa inläggen baserat på söktermen
+    filterPosts(searchTerm);
